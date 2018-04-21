28a1842c7e78c1db8927ab3c6ac584251cb373f8
Joshua Trimble <j.trimble@Joshuas-MBP.attlocal.net>
2018-04-21 08:12:48 -0500

    revised files

---
+++ assets/javascript/game.js
@@ -0,0 +1,146 @@
+//RELOAD PAGE
+function refreshPage() {
+var html = 
+        "<p>Press any key to get started!" +
+        "<p>Wins: " + wins + "</p>" +
+        "Current Word: <br>" + answerArray.join(' ') + "<br>" +
+        "Letters Guessed: " + wrongLetters +
+    
+        "<p>Guesses Remaining: " + guessesLeft + "<p>";
+    
+    // Set the inner HTML contents of the game div to my html
+    document.querySelector("#stats").innerHTML = html;
+}
+
+//GAME RESET -- LOSS
+function loseGame() {
+    location.reload();
+}
+
+//GAME RESET -- WIN
+function winGame() {
+    location.reload();
+    // refreshPage();
+    // answerArray = [];
+    // wrongLetters = [];
+    // guessesLeft = 12;
+    // wins+= 1;
+    
+
+//     $(wins).innerHTML(wins);
+//     correctLetters = [];
+//     $(guessesLeft).innerHTML(guessesLeft);
+//     alert("YOU WIN!!! PRESS 'OK' TO PLAY AGAIN!");
+//     // startGame();
+}
+
+// CHECK LETTERS REMAINING FOR WIN
+function checkWin() {
+    if(answerArray.indexOf("__ ") === -1) {
+        alert("YOU WIN! PRESS 'OK' TO TRY AGAIN!")
+        winGame();
+    } else if (guessesLeft === 0) {
+        alert("...you lost! PRESS 'OK' TO TRY AGAIN!");
+        loseGame();
+    }
+}
+
+//GAME LOOP!!!!
+var computerChoices;
+var computerGuess;
+var userChoices;
+var answerArray;
+var wrongLetters;
+var wins = 0;
+// var maxTries = 12; 
+var computerChoices = ["humble", "badandboujee", "unforgettable", "congratulations", "maskoff", "bodakyellow", "blackbeatles", "starboy", "fakelove", "rockstar", "slippery", "dejavu", "guccigang"];
+// var gameStart = false;
+// var gameFinished = false;
+var userGuess;
+var guessesLeft;
+
+//START GAME
+$(document).ready(function startGame(){
+
+computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log(computerGuess);
+userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ]
+guessesLeft = 12;
+wrongLetters = [];
+answerArray = [];
+
+console.log("playing");
+    //LOOP TO REPLACE COMPUTER GUESS WITH UNDERSCORES
+    for (var i = 0; i < computerGuess.length; i++) {
+        computerGuess[i] = answerArray;
+        answerArray.push("__ ");
+        // console.log(answerArray);
+    }
+
+    function updateGuesses(userGuess) {
+        console.log("WOOOOOOOW");
+        
+
+        if (computerGuess.indexOf(userGuess) === -1 && wrongLetters.indexOf(userGuess) === -1) {
+            
+            wrongLetters.push(userGuess);
+            guessesLeft--;
+            console.log("HOLYMOLY");
+        } else { 
+                for (var j = 0; j < computerGuess.length; j++) {
+                    if (computerGuess[j] == userGuess) {
+                        answerArray[j] = userGuess;
+                    if (answerArray.indexOf(userGuess) === -1 ) {
+                        answerArray.push(userGuess);
+                        console.log("WAIT, WHAT");
+                    }
+                }
+            }
+
+        }
+            refreshPage();
+            checkWin();
+
+            console.log("THE ENDDD");
+
+    }
+
+    //RECORD KEY PRESSED
+    $("body").keyup(function(event) {
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        updateGuesses(userGuess);
+        console.log(userGuess);
+
+    })
+
+    var html = 
+        "<p>Press any key to get started!" +
+        "<p>Wins: " + wins + "</p>" +
+        "Current Word: <br>" + answerArray.join(' ') + "<br>" +
+        "Letters Guessed: " + wrongLetters +
+    
+        "<p>Guesses Remaining: " + guessesLeft + "<p>";
+        // "<p>Letters Already Guessed: " + wrong letters + "<p>";
+    
+    // Set the inner HTML contents of the game div to my html
+    document.querySelector("#stats").innerHTML = html;
+
+})
+        
+// END GAME LOOP!!!
+
+// SETTING PAGE HTML
+// // function refreshPage() {
+//     var html = 
+//         "<p>Press any key to get started!" +
+//         "<p>Wins: " + wins + "</p>" +
+//         "Current Word: <br>" + answerArray + "<br>" +
+//         "Letters Guessed: " + wrongLetters +
+    
+//         "<p>Guesses Remaining: " + guessesLeft + "<p>";
+//         // "<p>Letters Already Guessed: " + wrong letters + "<p>";
+    
+//     // Set the inner HTML contents of the game div to my html
+//     document.querySelector("#stats").innerHTML = html;
+    
+//     // }
